2f47cc396eafb182cab3a16ac920730fd3200cd4
tomoya ishigaki <steinwand.tomo@gmail.com>
2022-10-20 02:25:17 +0900

    fix bug

--- create_script.sh
+++ create_script.sh
@@ -3,4 +3,10 @@ echo '#!/usr/bin/env rust-script' > $1
 echo '//! ```cargo ' >> $1
 echo '//! [dependencies] ' >> $1
 echo '//! nix = "0.25.0" ' >> $1
-echo '//! ``` "' >> $1
+echo '//! ``` ' >> $1
+echo '' >> $1
+echo 'fn main() {' >> $1
+echo '' >> $1
+echo '}' >> $1
+
+chmod +x $1
