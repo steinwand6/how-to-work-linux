2f553c1ec3d1731f9646ef7325e7514d10473453
tomoya ishigaki <steinwand.tomo@gmail.com>
2022-10-21 03:56:34 +0900

    add main to 02-02

--- 02/fork-and-exec.rs
+++ 02/fork-and-exec.rs
@@ -1,21 +1,33 @@
 #!/usr/bin/env rust-script
 // cargo-deps: nix, libc
 
-use nix::{sys::wait::waitpid,unistd::{fork, ForkResult, getppid, getpid, execve}};
+use nix::{
+    sys::wait::waitpid,
+    unistd::{execve, fork, getpid, getppid, ForkResult},
+};
 use std::ffi::CString;
 
-match unsafe{fork()} {
-	Ok(ForkResult::Parent { child, .. }) => {
-		println!("親プロセス: pid={}, 子プロセスのpid={}", getpid(), child);
-		waitpid(child, None).unwrap();
-	}
-	Ok(ForkResult::Child) => {
-		let cmd = CString::new("/bin/echo").expect("CString::new failed");
-		let args = [CString::new("echo")?,
-					CString::new(format!("pid={} からこんにちは", getpid())).expect("CString::new failed")];
-		let env = CString::new("").expect("CString::new failed");
-		println!("子プロセス: pid={}, 親プロセスのpid={}", getpid(), getppid());
-		execve(&cmd, &args, &[env]).expect("execve failed");
-	}
-	Err(_) => unsafe { libc::_exit(1) }
+fn main() -> Result<(), Error<dyn Error>> {
+    match unsafe { fork() } {
+        Ok(ForkResult::Parent { child, .. }) => {
+            println!("親プロセス: pid={}, 子プロセスのpid={}", getpid(), child);
+            waitpid(child, None).unwrap();
+        }
+        Ok(ForkResult::Child) => {
+            let cmd = CString::new("/bin/echo").expect("CString::new failed");
+            let args = [
+                CString::new("echo")?,
+                CString::new(format!("pid={} からこんにちは", getpid()))
+                    .expect("CString::new failed"),
+            ];
+            let env = CString::new("").expect("CString::new failed");
+            println!(
+                "子プロセス: pid={}, 親プロセスのpid={}",
+                getpid(),
+                getppid()
+            );
+            execve(&cmd, &args, &[env]).expect("execve failed");
+        }
+        Err(_) => unsafe { libc::_exit(1) },
+    }
 }
