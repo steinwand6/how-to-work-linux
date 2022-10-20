10f4dc0823c4aedbe07996374ff00a30fd6960c0
tomoya ishigaki <steinwand.tomo@gmail.com>
2022-10-21 02:17:05 +0900

    fix 02-01

--- 02/fork.rs
+++ 02/fork.rs
@@ -1,17 +1,18 @@
 #!/usr/bin/env rust-script
 // cargo-deps: nix, libc
 
-use nix::{sys::wait::waitpid,unistd::{fork, ForkResult, write}};
+use nix::{sys::wait::waitpid,unistd::{fork, ForkResult, getppid, getpid}};
 
 match unsafe{fork()} {
    Ok(ForkResult::Parent { child, .. }) => {
-       println!("Continuing execution in parent process, new child has pid: {}", child);
+	   println!("親プロセス: pid={}, 子プロセスのpid={}", getpid(), child);
        waitpid(child, None).unwrap();
    }
    Ok(ForkResult::Child) => {
        // Unsafe to use `println!` (or `unwrap`) here. See Safety.
-       write(libc::STDOUT_FILENO, "I'm a new child process\n".as_bytes()).ok();
+       // write(libc::STDOUT_FILENO, "I'm a new child process\n".as_bytes()).ok();
+	   println!("子プロセス: pid={}, 親プロセスのpid={}", getpid(), getppid());
        unsafe { libc::_exit(0) };
    }
-   Err(_) => println!("Fork failed"),
+   Err(_) => unsafe { libc::_exit(1) }
 }
