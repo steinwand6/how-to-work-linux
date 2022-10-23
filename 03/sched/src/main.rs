b0ba624639bb6f32b376672b98ce73bc9f1e3f3a
tomoya ishigaki <steinwand.tomo@gmail.com>
2022-10-23 13:12:07 +0900

    add 03-03

--- 03/sched/src/main.rs
+++ 03/sched/src/main.rs
@@ -1,5 +1,13 @@
-use nix::{sched, unistd};
-use std::time;
+use nix::{
+    sched,
+    sys::wait,
+    unistd::{self, fork, ForkResult},
+};
+use std::time::{self, Duration};
+use std::{
+    fs::{File, OpenOptions},
+    io::Write,
+};
 
 const NLOOP_FOR_ESTIMATION: usize = 100000000;
 
@@ -26,16 +34,40 @@ fn main() -> Result<(), SchedError> {
     let mut cpu_set = nix::sched::CpuSet::new();
     cpu_set.set(concurrency).expect("failed set cuncurrency");
     sched::sched_setaffinity(unistd::Pid::from_raw(0), &cpu_set).expect("failed shced setaffinity");
-    estimate_loops_per_msec();
+    let nloop_per_msec = estimate_loops_per_msec();
+
+    let start = time::Instant::now();
+    for i in 0..concurrency {
+        match unsafe { fork() } {
+            Ok(ForkResult::Parent { .. }) => (),
+            Ok(ForkResult::Child) => {
+                child_fn(i, nloop_per_msec, start);
+            }
+            Err(_) => unsafe { libc::_exit(1) },
+        }
+    }
+    for _ in 0..concurrency {
+        wait::wait().expect("wait failed");
+    }
     Ok(())
 }
 
-fn estimate_loops_per_msec() {
+fn estimate_loops_per_msec() -> u128 {
     let start = time::Instant::now();
     for _ in 0..NLOOP_FOR_ESTIMATION {}
     let end = time::Instant::now();
-    println!(
-        "{:?}",
-        NLOOP_FOR_ESTIMATION as u128 / end.duration_since(start).as_millis()
-    );
+    NLOOP_FOR_ESTIMATION as u128 / end.duration_since(start).as_millis()
+}
+
+fn child_fn(n: usize, nloop: u128, start: time::Instant) {
+    let mut progress: [Duration; 100] = [Duration::ZERO; 100];
+    for i in 0..100 {
+        for _ in 0..nloop {}
+        progress[i] = time::Instant::now().duration_since(start);
+    }
+    let mut f = File::create(format!("{}.data", n)).expect("create file failed");
+    for i in 0..100 {
+        write!(f, "{}\t{}\n", progress[i].as_secs_f64() * 1000.0, i).expect("write file failed");
+    }
+    unsafe { libc::_exit(0) };
 }
