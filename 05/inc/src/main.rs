3e1358dd3741f8a7f92269b04746e7a6701f3952
tomoya ishigaki <steinwand.tomo@gmail.com>
2022-10-27 04:21:06 +0900

    refactoring

--- 05/inc/src/main.rs
+++ 05/inc/src/main.rs
@@ -4,13 +4,13 @@ use nix::fcntl::{flock, open, FlockArg, OFlag};
 use nix::sys::stat::Mode;
 use nix::unistd::{read, write};
 
-fn main() {
+fn main() -> Result<(), nix::Error> {
     let path = Path::new("count");
-    let fd = open(path, OFlag::O_RDWR, Mode::empty()).expect("open failed");
-    flock(fd, FlockArg::LockExclusive).expect("flock failed");
+    let fd = open(path, OFlag::O_RDWR, Mode::empty())?;
+    flock(fd, FlockArg::LockExclusive)?;
 
     let mut buf = [0u8; 8];
-    read(fd, &mut buf).expect("read failed");
+    read(fd, &mut buf)?;
     let content = buf
         .iter()
         .filter(|b| **b != 0)
@@ -22,7 +22,8 @@ fn main() {
     count += 1;
     let content = count.to_string().into_bytes();
 
-    let fd = open(path, OFlag::O_WRONLY | OFlag::O_TRUNC, Mode::empty()).expect("open failed");
-    write(fd, &content).expect("write failed");
-    flock(fd, FlockArg::Unlock).expect("unlock failed");
+    let fd = open(path, OFlag::O_WRONLY | OFlag::O_TRUNC, Mode::empty())?;
+    write(fd, &content)?;
+    flock(fd, FlockArg::Unlock)?;
+    Ok(())
 }
