1c960071f8696f30bbd0af666c6fe5569abd7538
tomoya ishigaki <steinwand.tomo@gmail.com>
2022-10-29 14:52:21 +0900

    add 08-01

---
+++ 08/cache/src/main.rs
@@ -0,0 +1,51 @@
+use std::{error::Error, fs::OpenOptions, io::Write, os::raw::c_void, ptr, time};
+
+use nix::{
+    self,
+    sys::mman::{mmap, MapFlags, ProtFlags},
+};
+
+const CACHE_LINE_SIZE: usize = 64;
+const NACCESS: usize = 128 * 1024 * 1024;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut file = OpenOptions::new()
+        .create(true)
+        .read(true)
+        .write(true)
+        .open("out.txt")?;
+
+    let mut i = 2.0;
+    while i <= 16.0 {
+        let bufsize: usize = 2_f64.powf(i) as usize * 1024;
+        let data: *mut c_void = unsafe {
+            mmap(
+                ptr::null_mut(),
+                bufsize,
+                ProtFlags::PROT_READ | ProtFlags::PROT_WRITE,
+                MapFlags::MAP_SHARED | MapFlags::MAP_ANON,
+                -1,
+                0,
+            )?
+        };
+
+        println!(
+            "バッファサイズ 2^{:.2}({}) KBについてのデータを収集中...",
+            i,
+            bufsize / 1024
+        );
+        let start = time::Instant::now();
+        let trytimes = NACCESS / (bufsize / CACHE_LINE_SIZE);
+        for _ in 0..trytimes {
+            for j in (0..bufsize).skip(CACHE_LINE_SIZE) {
+                unsafe {
+                    (data.add(j) as *mut u8).write(0);
+                }
+            }
+        }
+        let end = time::Instant::now().duration_since(start).as_nanos();
+        writeln!(&mut file, "{}\t{}", i, (NACCESS as f64 / end as f64))?;
+        i += 0.25;
+    }
+    Ok(())
+}
